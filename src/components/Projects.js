42310d9f49511f3947f9e66f6a89cd1f703a562e
toriecr <torie.cr@gmail.com>
2020-10-21 17:31:42 -0700

    Add nav, projects, experience

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,29 @@ import React from "react"
 import Title from "./Title"
 import Project from "./Project"
 import { Link } from "gatsby"
-const Projects = () => {
-  return <h2>projects list</h2>
+
+//{projects, title, showLink}
+const Projects = (props) => {
+  return (
+    <section className="section projects">
+      <Title title={props.title} />
+      <div className="section-center projects-center">
+        {
+          props.projects.map((project, index)=>{
+            return <Project key={project.id} index={index} {...project} />
+          })
+        }
+      </div>
+      {
+        props.showLink && (
+          <Link to="/projects" className="btn center-btn">
+            projects
+          </Link>
+        )
+      }
+    </section>
+  )
+  
 }
 
 export default Projects
